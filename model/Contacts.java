dd5455bbadc70ccd2474c5a3becfdd4b0b657519
marcus.munzert@generative-software.com <help@genmymodel.com>
2015-05-19 09:28:29 +0000

    commit by marcus.munzert@generative-software.com using genmymodel: v20150519_092828

---
+++ model/Contacts.java
@@ -0,0 +1,23 @@
+package model;
+
+
+/**
+ * <br>
+ * <!-- begin-user-doc -->
+ * <!--  end-user-doc  -->
+ * @generated
+ */
+
+public class Contacts
+{
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!--  end-user-doc  -->
+	 * @generated
+	 */
+	public Contacts(){
+		super();
+	}
+
+}
+
